09cfae270a834918f0ac26a5d858f5a921dc4530
jupiter@pro16 <zcsjupiter@126.com>
2021-10-27 14:50:22 -0400

    crawler

---
+++ crawler/transcript_single.py
@@ -0,0 +1,8 @@
+transcript_dict = {
+    'Season': 1,
+    'Episode': 1,
+    'Title': 'Pilot',
+    'Actor2Line': dict(list()), # ActorName: list((idx, ActorLine))
+    'Actor2Profile': dict(), # if exist, ActorName: url
+    'AllLines': list() # list((idx, Line))
+}
