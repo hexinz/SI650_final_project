78e2cacd4d16ab0b89c0bf6da625329d21608362
jupiter@pro16 <zcsjupiter@126.com>
2021-10-30 19:48:05 -0400

    Add missing transcripts

--- crawler/craw_season.py
+++ crawler/craw_season.py
@@ -1,40 +1,38 @@
 from craw_single_page import Crawler
 import requests
 from bs4 import BeautifulSoup
-season=1
-
-url = 'https://bigbangtheory.fandom.com/wiki/Season_1'
-
-titles = ['']
-
-crawler = Crawler()
-
 
 if __name__ == '__main__':
     
-    sublinks = set()
+    for season in range(2,2+1):
 
-    r = requests.get(url)
-    r.encoding = r.apparent_encoding
-    html = r.text
-    soup = BeautifulSoup(html, 'html.parser')
-    td = soup.find_all('td')
-    for dummy in td:
-        links = dummy.find_all('a')
-        for link in links:
-            link = link.get('href')
-            if link is not None:
-                sublinks.add(link)
-    
-    for i, link in enumerate(sublinks):
-        if link.startswith('/wiki/'):
-            subname = link[6:].rstrip()
-            print(subname)
+        url = 'https://bigbangtheory.fandom.com/wiki/Season_'+str(season)
 
-            try:
-                url = 'https://bigbangtheory.fandom.com/wiki/Transcripts/'+subname
-                crawler.craw_page(url, season, i, subname)
+        crawler = Crawler()
 
-            except Exception as e:
-                print(e)
+        sublinks = set()
+
+        r = requests.get(url)
+        r.encoding = r.apparent_encoding
+        soup = BeautifulSoup(r.text, 'html.parser')
+        td = soup.find_all('td')
+
+        for dummy in td:
+            links = dummy.find_all('a') # get all usable links
+            for link in links:
+                link = link.get('href')
+                if link is not None:
+                    sublinks.add(link)
+        
+        for link in sublinks:
+            if link.startswith('/wiki/'):
+                subname = link[6:].rstrip()
+                print(subname)
+
+                try:
+                    url = 'https://bigbangtheory.fandom.com/wiki/Transcripts/'+subname
+                    crawler.craw_page(url, season, subname)
+
+                except Exception as e:
+                    print(e)
 
--- crawler/craw_single_page.py
+++ crawler/craw_single_page.py
@@ -4,8 +4,16 @@ import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
 import json
+import os
+import re
 
-url = 'https://bigbangtheory.fandom.com/wiki/Transcripts/Pilot'
+url = 'https://bigbangtheory.fandom.com/wiki/Transcripts/'
+
+def clean_me(html):
+    soup = BeautifulSoup(html)
+    for s in soup(['script', 'style']):
+        s.decompose()
+    return ' '.join(soup.stripped_strings)
 
 class Crawler():
     
@@ -13,9 +21,16 @@ class Crawler():
         pass
         # self.base_url = base_url
 
-    def craw_page(self, url, season, episode, title):
+    def craw_page(self, url, season, title):
 
         # page_name = os.path.basename(url)
+        r = requests.get(url)
+        r.encoding = r.apparent_encoding
+        html = r.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        regex = r"-_Episode_([0-9]{1,2})_-"
+        episode = 6#int(re.findall(regex, html)[0])
 
         transcript_dict = {
             'Season': 1,
@@ -26,11 +41,6 @@ class Crawler():
             'AllLines': list()
         }
 
-        r = requests.get(url)
-        r.encoding = r.apparent_encoding
-        html = r.text
-        soup = BeautifulSoup(html, 'html.parser')
-
         br = soup.find_all('tbody')
         transcript = br[0]
 
@@ -40,21 +50,41 @@ class Crawler():
         for pair in transcript_data[1:]:
             
             actor, content = pair.find_all('td')
-            if content.string is None:
-                continue
 
+            # get content
+            if content.string is None:
+                if len(content.find_all('i')) > 0:
+                    try:
+                        actor_line = content.i.string.rstrip()
+                    except AttributeError:
+                        try:
+                            actor_line = content.a.string + content.i.contents[1]
+                        except TypeError:
+                            actor_line = "" 
+                            # print(content.find_all('i')[0].contents)
+                            for item in content.find_all('i')[0].contents:
+                                if item.string is not None:
+                                    actor_line += item.string.rstrip()
+                                if isinstance(item, str):
+                                    actor_line += item
+                else:
+                    continue
+            else:
+                actor_line = content.string.rstrip()
+
+            # get actor
             if len(actor.find_all('a')) > 0:
                 actor_name = actor.a.string.rstrip()
                 actor_link = str(actor.a.get('href'))
                 transcript_dict['Actor2Profile'][actor_name] = actor_link
             else:
-                actor_name = actor.string.rstrip()
-                
-            actor_line = content.string.rstrip()
-            
-            assert isinstance(actor_name, str)
-            assert isinstance(actor_line, str)
-            
+                if actor.string is not None:
+                    actor_name = actor.string.rstrip()
+                    if actor_name == "":
+                        actor_name = "Scene"
+                else:
+                    actor_name = "Scene"
+
             if actor_name not in transcript_dict['Actor2Line']:
                 transcript_dict['Actor2Line'][actor_name] = [(line_idx, actor_line)]
             else:
@@ -67,5 +97,12 @@ class Crawler():
             #    print(e)
         # print(transcript_dict)
 
-        with open('transcripts/'+title+'.json', 'w+') as fout:
+        save_path = os.path.join('transcripts', 'season'+str(season), title+'.json')
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+        with open(save_path, 'w+') as fout:
             json.dump(transcript_dict, fout, indent=4)
+
+if __name__ == '__main__':
+    crawler = Crawler()
+    crawler.craw_page(url+'The Cooper-Nowitzki Theorem', 2, 'The Cooper-Nowitzki Theorem')
